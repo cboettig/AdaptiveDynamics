73c0596108e7a6cca21efdb226fc76980e6f863b
Carl Boettiger <cboettig@gmail.com>
2010-04-30 15:01:14 -0700

    first analytic approx looking good

--- R/analytics.R
+++ R/analytics.R
@@ -1,9 +1,9 @@
 # analytics.R
-MINF <- -.25 
-INF <- .25
-MAXRES <- 50
+MINF <- -1 
+INF <- 1
+MAXRES <- 1000
 
-pars <- list(sigma_mu = 0.02, sigma_c2 = .1, sigma_k2 = 1, ko = 100, mu = 0.001, xo = 0.2)
+pars <- list(sigma_mu = 0.01, sigma_c2 = .1, sigma_k2 = 1, ko = 100, mu = 0.01, xo = 0.5)
 
 
 ## Functions as defined in writeup
@@ -15,50 +15,45 @@ phi <- function(x, pars){ x*(pars$sigma_k2/pars$sigma_c2+1)/(pars$sigma_k2/pars$
 psi <- function(x, pars){ x*(pars$sigma_k2/pars$sigma_c2-1)/(pars$sigma_k2/pars$sigma_c2+1) }
 
 # We only want to calculate these once
-reals <- seq(MINF, INF, length.out = MAXRES)
-coexist_with_x <- function(x, pars){   reals[ (reals < phi(x,pars) | reals > psi(x,pars) ) ] }
+coexist_with_x <- function(x, pars, reals){   reals[ (reals < phi(x,pars) | reals > psi(x,pars) ) ] }
 
-P <- function(x, pars){
-	y <- coexist_with_x(x, pars)
+P <- function(x, pars, reals){
+	y <- coexist_with_x(x, pars, reals)
 	deltaX <- y[2] - y[1]
 	K(x,pars)*pars$mu*sum( sapply(y, function(y){ S(y,x,pars)*M(y,x,pars)*deltaX } ) )
 }
+reals <- seq(MINF, INF, length.out = MAXRES)
+P(.04, pars, reals)
 
-P(.04, pars)
-
-
-P_matrix 
-S_x_M  <- function(x){ sapply(reals, function(y){ S(y,x, pars)*M(y,x,pars) } ) }
-SxM_matrix <- sapply(reals, S_x_M)
-
-P <- function(x, pars=pars){
-	step <- reals[2]-reals[1]
-	x_loc <- (reals > x & reals <= x + step)
-	pars$mu*K(x,pars)*( sum(SxM_matrix[reals < phi(x,pars),x_loc ] ) + sum(SxM_matrix[reals > psi(x,pars), x_loc ] ) )
-}
 
 # canonical path
 ## names makes this easier to read, but may slow down execution.
-times <- seq(0, 1e4, length.out=MAXRES);
+times <- seq(0, 5e4, length.out=MAXRES);
 f <- function(t, y, p)
   {
 	list(-y * 0.5 * p$mu * p$sigma_mu^2 * K(y,p) / p$sigma_k2)
   }
 require(odesolve)
-meanpath <- lsoda(2,times,f, pars, rtol=1e-4, atol= 1e-6)
+meanpath <- lsoda(pars$xo,times,f, pars, rtol=1e-4, atol= 1e-6)
 #plot(meanpath)
 tstep = times[2] - times[1]
 
-P_path <- P(meanpath[,2], pars) 
+P_path <- sapply(meanpath[,2], function(x) P(x,pars, reals) )
+#plot(P_path)
+
+
 integral <- cumsum(P_path*tstep)
 
 myPi <- function(Time){
 	Time_loc <- which(times > Time & times < Time + tstep)
 	P_path[Time_loc] * exp(-integral[Time_loc])
 }
-ti <- seq(1,1e3, len=50)
+
+myPi(500)
+
+ti <- seq(1,5e3, len=MAXRES/2)
 dist <- sapply(ti, myPi)
-plot(ti, dist)
-sum(dist*ti)/sum(dist)
+#plot(ti, dist)
+print( sum(dist*ti)/sum(dist) )
 
 
