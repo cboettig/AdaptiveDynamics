e955bb8957e2857fbe5116b0550d29c7a84ef349
Carl Boettiger <cboettig@gmail.com>
2010-12-29 15:32:13 -0800

    autocommit

--- demos/waiting_times.R
+++ demos/waiting_times.R
@@ -10,28 +10,39 @@ log <- gitlog()
 #			5  = Number of times the dimporphism is lost in phase 2 (in a single run, until reaching theshhold)
 #			6  = Number of times the dimoprhism is lost in phase 1 (in a single run, until reaching theshhold)
 
-rep <- 160
+rep <- 16
 cpu <- 16
 K <- 10
 
 vary_sigma_c2 <- lapply(1:K, function(i){
-	sigma_c2 <- seq(.1, .8, length=10)
-	ensemble_sim(rep=rep, sigma_mu = 0.05, mu = 5e-3, sigma_c2 = sigma_c2[i], sigma_k2 = 1, ko = 500, xo = 0.1, threshold = 30, cpu=cpu) 
+	sigma_c2 <- seq(.1, .8, length=K)
+	ensemble_sim(rep=rep, sigma_mu = 0.05, mu = 5e-4, sigma_c2 = sigma_c2[i], sigma_k2 = 1, ko = 500, xo = 0.1, threshold = 30, cpu=cpu) 
 	})
+	sigma_c2 <- seq(.1, .8, length=K)
+
+plot_phases(vary_sigma_c2, sigma_c2)
 
 vary_sigma_mu <- lapply(1:K, function(i){
 	sigma_mu  <- seq(.02, .1, length=K)
-	ensemble_sim(rep=rep, sigma_mu = sigma_mu[i], mu = 5e-3, sigma_c2 = 0.3, sigma_k2 = 1, ko = 500, xo = 0.1, threshold = 30, cpu=cpu) 
+	ensemble_sim(rep=rep, sigma_mu = sigma_mu[i], mu = 5e-4, sigma_c2 = 0.3, sigma_k2 = 1, ko = 500, xo = 0.1, threshold = 30, cpu=cpu) 
 	})
+	sigma_mu  <- seq(.02, .1, length=K)
+plot_phases(vary_sigma_mu, sigma_mu)
+
+
 
 vary_mu <- lapply(1:K, function(i){
 	mu <- seq(1e-4, 1e-2, length=K)
 	ensemble_sim(rep=rep, sigma_mu = 0.05, mu =mu[i], sigma_c2 = 0.3, sigma_k2 = 1, ko = 500, xo = 0.1, threshold = 30, cpu=cpu) 
 	})
+	mu <- seq(1e-4, 1e-2, length=K)
+plot_phases(vary_mu, mu)
 
-save(list=ls(), file="waiting_time.Rdat")
 
-system("git add waiting_time.Rdat")
+
+save(list=ls(), file="waiting_time2.Rdat")
+
+system("git add waiting_time2.Rdat")
 gitcommit()
 log <- gitlog()
 system("git push")
