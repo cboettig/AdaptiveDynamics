54d19b216b18313a79ad9acb692a3d35a71a5829
Carl Boettiger <cboettig@gmail.com>
2010-12-29 20:39:00 -0800

    autocommit

--- demos/phases_plots.R
+++ demos/phases_plots.R
@@ -30,25 +30,25 @@ plot_phases <- function(all, parameter, xlab){
 	)
 
 
-	plot_err <- function(phase, xlab, add=F, col="black", ylab=ylab){
+	plot_err <- function(phase, xlab, add=F, col="black", ylab=ylab, ...){
 		cm1 <- colMeans(phase)
 		cv1 <- sqrt(colVars(phase))
-			errbar(parameter, cm1, cm1+cv1, cm1-cv1, xlab=xlab, ylab=ylab, add=add, col=col)
-			lines(parameter, cm1, col=col)
+			errbar(parameter, cm1, cm1+cv1, cm1-cv1, xlab=xlab, ylab=ylab, add=add, col=col, ...)
+			lines(parameter, cm1, col=col, ...)
 	}
 
 	plottries <- function(){
 		plot_err(phase1_attempts, xlab, ylab="Num. of attempts")
 		plot_err(phase2_attempts, add=T, col="blue")
-		legend("topleft", c("phase 1", "phase 2"), col=c("black", "blue"))
+		legend("topleft", c("phase 1", "phase 2"), col=c("black", "blue"), lty=1)
 	}
 
 	
 	plottimes <- function(){
-	plot_err(phase3_times, xlab, ylab="Num of times")
+	plot_err(phase3_times, xlab, ylab="Num of times", lwd=3)
 	plot_err(phase2_times, xlab, add=T, col="blue")
 	plot_err(phase1_times, xlab, add=T, col="green")
-	legend("topleft", c("phase 3", "phase 2", "phase 1"), col=c("black", "blue", "green"))
+	legend("topleft", c("phase 3", "phase 2", "phase 1"), col=c("black", "blue", "green"), lty=1)
 }
 
 social_plot(plottries(), file="attempts.png", tags="adaptivedynamics", comment="Number of attempts from phase 1")
