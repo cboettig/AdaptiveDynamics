2210379d96ca4aca77de636dc5ebb93501d2cdfb
Carl Boettiger <cboettig@gmail.com>
2010-12-31 10:22:25 -0800

    trying bigger steps

--- demos/step_limited.R
+++ demos/step_limited.R
@@ -6,18 +6,17 @@ tags <- c("adaptivedynamics", "simulations")
 log <- gitlog()
 rep <- 16
 cpu <- 16
-K <- 10
+K <- 7
 
 
-#vary_mu <- lapply(1:K, function(i){
-#	mu <- seq(1e-4, 1e-3, length=K)
-#	ensemble_sim(rep=rep, sigma_mu = 0.05, mu =mu[i], sigma_c2 = 0.5, sigma_k2 = 1, ko = 500, xo = 0.1, threshold = 30, cpu=cpu, maxtime=5e6) 
-#	})
+vary_mu <- lapply(1:K, function(i){
+	mu <- seq(4e-4, 1e-3, length=K)
+	ensemble_sim(rep=rep, sigma_mu = 0.08, mu =mu[i], sigma_c2 = 0.5, sigma_k2 = 1, ko = 500, xo = 0.1, threshold = 30, cpu=cpu, maxtime=1e7) 
+	})
 
-#save(list=ls(), file="step_limited.Rdat")
-load("step_limited.Rdat")
+save(list=ls(), file="step_limited.Rdat")
 source("phases_plots.R")
-mu <- seq(1e-4, 1e-3, length=K)
+mu <- seq(4e-4, 1e-3, length=K)
 plot_phases(vary_mu, mu, xlab="mu")
 
 
