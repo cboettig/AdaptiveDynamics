2207ddc0e6652bef934d31a2dbc8f1e01c3f91ef
Carl Boettiger <cboettig@gmail.com>
2010-12-31 07:13:25 -0800

    autocommit

--- demos/phases_plots.R
+++ demos/phases_plots.R
@@ -1,6 +1,5 @@
 colVars <- function(X) sapply(1:dim(X)[2], function(i) var(X[,i]))
 library(Hmisc)
-
 ##
 # plot the output of waiting_times.R
 # all is one of vary_mu, vary_sigma_mu, etc
@@ -31,28 +30,30 @@ plot_phases <- function(all, parameter, xlab){
 	)
 
 
-	plot_err <- function(phase, xlab, add=F, col="black", ylab=ylab){
+	plot_err <- function(phase, xlab, add=F, col="black", ylab=ylab, ...){
 		cm1 <- colMeans(phase)
 		cv1 <- sqrt(colVars(phase))
-			errbar(parameter, cm1, cm1+cv1, cm1-cv1, xlab=xlab, ylab=ylab, add=add, col=col)
-			lines(parameter, cm1)
+			errbar(parameter, cm1, cm1+cv1, cm1-cv1, xlab=xlab, ylab=ylab, add=add, col=col, ...)
+			lines(parameter, cm1, col=col, ...)
 	}
 
-	plotme <- function(){
+	plottries <- function(){
 		plot_err(phase1_attempts, xlab, ylab="Num. of attempts")
 		plot_err(phase2_attempts, add=T, col="blue")
+		legend("topleft", c("phase 1", "phase 2"), col=c("black", "blue"), lty=1)
 	}
 
 	
-	plotme <- function(){
-	plot_err(phase1_times, xlab)
-	plot_err(phase2_times, xlab)
-	plot_err(phase3_times, xlab)
-	}
+	plottimes <- function(){
+	plot_err(phase3_times, xlab, ylab="Num of times", lwd=3)
+	plot_err(phase2_times, xlab, add=T, col="blue")
+	plot_err(phase1_times, xlab, add=T, col="green")
+	legend("topleft", c("phase 3", "phase 2", "phase 1"), col=c("black", "blue", "green"), lty=1)
+}
 
-social_plot(plotme(),
-		file="attempts.png", tags="adaptivedynamics", comment="Number of attempts from phase 1")
-	
+social_plot(plottries(), file="attempts.png", tags="adaptivedynamics", comment="Number of attempts from phase 1")
+social_plot(plottimes(), file="times.png", tags="adaptivedynamics", comment="Number of attempts from phase 1")
+
 }
 #social_plot(errbar(sigma_c2, cm1/cm2, cm1/cm2+(cv1+cv2), cm1/cm2-(cv1+cv2)),
 #	file="sigma_c2.png", tags="adaptivedynamics", comment="ratio")
--- demos/step_limited.R
+++ demos/step_limited.R
@@ -9,14 +9,15 @@ cpu <- 16
 K <- 10
 
 
-vary_mu <- lapply(1:K, function(i){
-	mu <- seq(1e-4, 1e-3, length=K)
-	ensemble_sim(rep=rep, sigma_mu = 0.05, mu =mu[i], sigma_c2 = 0.5, sigma_k2 = 1, ko = 500, xo = 0.1, threshold = 30, cpu=cpu, maxtime=5e6) 
-	})
+#vary_mu <- lapply(1:K, function(i){
+#	mu <- seq(1e-4, 1e-3, length=K)
+#	ensemble_sim(rep=rep, sigma_mu = 0.05, mu =mu[i], sigma_c2 = 0.5, sigma_k2 = 1, ko = 500, xo = 0.1, threshold = 30, cpu=cpu, maxtime=5e6) 
+#	})
 
-save(list=ls(), file="step_limited.Rdat")
+#save(list=ls(), file="step_limited.Rdat")
+load("step_limited.Rdat")
 source("phases_plots.R")
 mu <- seq(1e-4, 1e-3, length=K)
-plot_phases(vary_mu, sigma_c2, xlab="mu")
+plot_phases(vary_mu, mu, xlab="mu")
 
 
