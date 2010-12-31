37aebd6aa4c38935d63f70db37f5bbbf54811171
Carl Boettiger <cboettig@gmail.com>
2010-12-30 22:09:51 -0800

    added step_limited example to test

--- demos/phases_plots.R
+++ demos/phases_plots.R
@@ -1,4 +1,5 @@
 colVars <- function(X) sapply(1:dim(X)[2], function(i) var(X[,i]))
+library(Hmisc)
 
 ##
 # plot the output of waiting_times.R
@@ -30,30 +31,28 @@ plot_phases <- function(all, parameter, xlab){
 	)
 
 
-	plot_err <- function(phase, xlab, add=F, col="black", ylab=ylab, ...){
+	plot_err <- function(phase, xlab, add=F, col="black", ylab=ylab){
 		cm1 <- colMeans(phase)
 		cv1 <- sqrt(colVars(phase))
-			errbar(parameter, cm1, cm1+cv1, cm1-cv1, xlab=xlab, ylab=ylab, add=add, col=col, ...)
-			lines(parameter, cm1, col=col, ...)
+			errbar(parameter, cm1, cm1+cv1, cm1-cv1, xlab=xlab, ylab=ylab, add=add, col=col)
+			lines(parameter, cm1)
 	}
 
-	plottries <- function(){
+	plotme <- function(){
 		plot_err(phase1_attempts, xlab, ylab="Num. of attempts")
 		plot_err(phase2_attempts, add=T, col="blue")
-		legend("topleft", c("phase 1", "phase 2"), col=c("black", "blue"), lty=1)
 	}
 
 	
-	plottimes <- function(){
-	plot_err(phase3_times, xlab, ylab="Num of times", lwd=3)
-	plot_err(phase2_times, xlab, add=T, col="blue")
-	plot_err(phase1_times, xlab, add=T, col="green")
-	legend("topleft", c("phase 3", "phase 2", "phase 1"), col=c("black", "blue", "green"), lty=1)
-}
+	plotme <- function(){
+	plot_err(phase1_times, xlab)
+	plot_err(phase2_times, xlab)
+	plot_err(phase3_times, xlab)
+	}
 
-social_plot(plottries(), file="attempts.png", tags="adaptivedynamics", comment="Number of attempts from phase 1")
-social_plot(plottimes(), file="times.png", tags="adaptivedynamics", comment="Number of attempts from phase 1")
-
+social_plot(plotme(),
+		file="attempts.png", tags="adaptivedynamics", comment="Number of attempts from phase 1")
+	
 }
 #social_plot(errbar(sigma_c2, cm1/cm2, cm1/cm2+(cv1+cv2), cm1/cm2-(cv1+cv2)),
 #	file="sigma_c2.png", tags="adaptivedynamics", comment="ratio")
